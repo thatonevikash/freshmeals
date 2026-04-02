321dda9181c4976058d0ebbcc07f2eabbe0d465c
codezaura <vikashloharjsr@gmail.com>
2026-04-02 21:37:18 +0530

    fix: routes and controller have been organized

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -17,37 +17,3 @@ export async function getUser(req: Request, res: Response) {
 // -------------------------------------------------------------
 
 export async function updateUser(req: Request, res: Response) {}
-
-// -------------------------------------------------------------
-
-export async function registerSeller(req: Request, res: Response) {
-  const seller = await User.findByIdAndUpdate(
-    req.userId,
-    { is_registered_seller: true },
-    { new: true },
-  ).select("-password");
-
-  if (!seller) {
-    res.status(404).json({ message: "User not found" });
-    return;
-  }
-
-  res.status(200).json({ message: "Seller registered", data: seller });
-}
-
-// -------------------------------------------------------------
-
-export async function unregisterSeller(req: Request, res: Response) {
-  const seller = await User.findByIdAndUpdate(
-    req.userId,
-    { is_registered_seller: false },
-    { new: true },
-  ).select("-password");
-
-  if (!seller) {
-    res.status(404).json({ message: "User not found" });
-    return;
-  }
-
-  res.status(200).json({ message: "Seller unregistered", data: seller });
-}
--- server/src/routes/meal.routes.ts
+++ server/src/routes/meal.routes.ts
@@ -5,6 +5,24 @@ import * as Controller from "../controllers/meal.controller";
 
 const router = Router();
 
+router.get("/", Controller.getMeals);
+
+router.post("/", Controller.createMeal);
+
+router.get("/plate", Controller.getMealPlates);
+
+router.post("/plate", Controller.createMealPlate);
+
+router.get("/collection", Controller.getMealCollection);
+
+router.get("/plate/:plate_id", Controller.getMealPlate);
+
+router.put("/plate/:plate_id", Controller.updateMealPlate);
+
+router.patch("/plate/:plate_id/add/:meal_id", Controller.addPlateItem);
+
+router.patch("/plate/:plate_id/remove/:meal_id", Controller.removePlateItem);
+
 router.post("/order", Controller.orderMeal);
 
 router.get("/recent_ordered", Controller.getRecentOrderedMeals);
@@ -16,5 +34,9 @@ router.get("/most_ordered/plate", Controller.getMostOrderedPlates);
 router.get("/plate/:plate_id", Controller.getMealPlate);
 
 router.get("/:meal_id", Controller.getMeal);
+
+router.put("/:meal_id", Controller.updateMeal);
+
+router.delete("/:meal_id", Controller.deleteMeal);
 
 export default router;
